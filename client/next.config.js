d9889bc83664cde26b7e121af022d3b336d4d541
brandon600 <us-guest@us-guests-MacBook-Pro.local>
2023-10-05 15:13:56 -0500

    heroku keys updates

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,4 @@
-const keys = process.env.NODE_ENV === 'production' ? require('./src/config/keys_prod') : require('./src/config/keys_dev');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        ...keys
-    },
-}
+const nextConfig = {}
 
-module.exports = nextConfig;
-
+module.exports = nextConfig
--- client/src/config/keys_prod.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    NEXT_PUBLIC_publicKey: 'process.env.PUBLIC_KEY'
-}
