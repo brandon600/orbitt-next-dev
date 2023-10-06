3ae7985a44a068acc96dae8386e3fb8fb70adb60
brandon600 <us-guest@us-guests-MacBook-Pro.local>
2023-10-05 19:49:08 -0500

    wrapped next around express

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,6 @@
 module.exports = {
     mongoURI:process.env.MONGO_URI,
     accountSid:process.env.ACCOUNT_SID_URI,
-    authToken:process.env.AUTH_TOKEN_URI
+    authToken:process.env.AUTH_TOKEN_URI,
+    localLink:process.env.NEXT_PUBLIC_API_BASE_URL
 }
