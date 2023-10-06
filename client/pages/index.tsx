4723b26bfad8e57b5f9ac4fdb32b38625ec55389
brandon600 <us-guest@us-guests-MacBook-Pro.local>
2023-10-06 16:38:02 -0500

    redirect and memberstack live accoount added

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>;
-  }
-  
-  export default HomePage;
+  const router = useRouter();
+
+  // Redirect to the '/dashboard' page when the component mounts
+  useEffect(() => {
+    router.push('/dashboard');
+  }, []); // The empty dependency array ensures this effect runs only once when the component mounts
+
+  return <div>Redirecting to Dashboard...</div>;
+}
+
+export default HomePage;
